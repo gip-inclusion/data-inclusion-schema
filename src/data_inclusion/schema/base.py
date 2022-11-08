aacfdb8dfe023e6c7db245e84e8b4f228296bb34
Valentin Matton <taq@protonmail.ch>
2022-11-08 19:28:49 +0100

    nit

--- src/data_inclusion/schema/base.py
+++ src/data_inclusion/schema/base.py
@@ -6,16 +6,16 @@ class EnhancedEnum(str, enum.Enum):
     def __new__(cls, value, label, description):
         obj = str.__new__(cls, [value])
         obj._value_ = value
-        obj.label = label
-        obj.description = (
+        obj._label = label
+        obj._description = (
             textwrap.dedent(description).strip("\n").replace("\n", " ")
             if description
             else None
         )
 
         try:
-            if obj.description is not None:
-                if not obj.description[0].isupper():
+            if obj._description is not None:
+                if not obj._description[0].isupper():
                     raise ValueError("description must start with uppercase letter")
         except ValueError as err:
             raise ValueError(f"{cls.__name__}({value})'s {err}")
@@ -35,3 +35,11 @@ class EnhancedEnum(str, enum.Enum):
             ],
             key=lambda d: d["value"],
         )
+
+    @property
+    def label(self) -> str:
+        return self._label
+
+    @property
+    def description(self) -> str:
+        return self._description
