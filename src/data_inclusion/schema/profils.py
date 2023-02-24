ef36425819e2dd494d05319e557dea2fa8fa189c
Valentin Matton <taq@protonmail.ch>
2023-02-24 12:16:27 +0100

    chore: split models module

---
+++ src/data_inclusion/schema/profils.py
@@ -0,0 +1,62 @@
+from data_inclusion.schema.base import EnhancedEnum
+
+
+class Profil(EnhancedEnum):
+    SENIORS_65 = (
+        "seniors-65",
+        "Seniors (+ 65 ans)",
+        None,
+    )
+    FAMILLES_ENFANTS = (
+        "familles-enfants",
+        "Familles/enfants",
+        None,
+    )
+    ADULTES = (
+        "adultes",
+        "Adultes",
+        None,
+    )
+    JEUNES_16_26 = (
+        "jeunes-16-26",
+        "Jeunes (16-26 ans)",
+        None,
+    )
+    PUBLIC_LANGUES_ETRANGERES = (
+        "public-langues-etrangeres",
+        "Public langues étrangères",
+        None,
+    )
+    DEFICIENCE_VISUELLE = (
+        "deficience-visuelle",
+        "Déficience visuelle",
+        None,
+    )
+    SURDITE = (
+        "surdite",
+        "Surdité",
+        None,
+    )
+    HANDICAPS_PSYCHIQUES = (
+        "handicaps-psychiques",
+        (
+            "Handicaps psychiques : troubles psychiatriques donnant lieu à des"
+            " atteintes comportementales"
+        ),
+        None,
+    )
+    HANDICAPS_MENTAUX = (
+        "handicaps-mentaux",
+        "Handicaps mentaux : déficiences limitant les activités d’une personne",
+        None,
+    )
+    FEMMES = (
+        "femmes",
+        "Femmes",
+        "Le lieu propose des accompagnements réservés aux femmes.",
+    )
+    PERSONNES_EN_SITUATION_ILLETTRISME = (
+        "personnes-en-situation-illettrisme",
+        "Personnes en situation d’illettrisme",
+        None,
+    )
