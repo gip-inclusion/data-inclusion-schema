6743962993f7c91ceec05ed1bbe7bb99d7a50413
Valentin Matton <taq@protonmail.ch>
2023-08-01 17:12:21 +0200

    chore: upgrade pydantic

--- src/data_inclusion/schema/models.py
+++ src/data_inclusion/schema/models.py
@@ -2,7 +2,8 @@ from datetime import date, datetime
 from typing import Optional
 
 import pydantic
-from pydantic import BaseModel, EmailStr, Extra, HttpUrl, constr
+from pydantic import BaseModel, ConfigDict, EmailStr, HttpUrl, StringConstraints
+from typing_extensions import Annotated
 
 from data_inclusion.schema.code_officiel_geographique import TypeCOG
 from data_inclusion.schema.frais import Frais
@@ -19,90 +20,116 @@ from data_inclusion.schema.typologies_de_structures import Typologie
 
 
 class Service(BaseModel):
+    model_config = ConfigDict(extra="forbid")
+
+    # fields
     id: str
-    structure_id: Optional[str]
+    structure_id: Optional[str] = None
     source: str
     nom: str
-    presentation_resume: Optional[constr(max_length=280)]
-    presentation_detail: Optional[str]
-    types: Optional[list[TypologieService]]
-    thematiques: Optional[list[Thematique]]
-    prise_rdv: Optional[HttpUrl]
-    frais: Optional[list[Frais]]
-    frais_autres: Optional[str]
-    profils: Optional[list[Profil]]
-    pre_requis: Optional[str]
-    cumulable: Optional[bool]
-    justificatifs: Optional[str]
-    formulaire_en_ligne: Optional[HttpUrl]
-    commune: Optional[str]
-    code_postal: Optional[constr(min_length=5, max_length=5, regex=r"^\d{5}$")]
-    code_insee: Optional[constr(min_length=5, max_length=5)]
-    adresse: Optional[str]
-    complement_adresse: Optional[str]
-    longitude: Optional[float]
-    latitude: Optional[float]
-    recurrence: Optional[str]
-    date_creation: Optional[date]
-    date_suspension: Optional[date]
-    lien_source: Optional[HttpUrl]
-    telephone: Optional[str]
-    courriel: Optional[EmailStr]
-    contact_public: Optional[bool]
-    date_maj: Optional[date | datetime]
-    modes_accueil: Optional[list[ModeAccueil]]
-    zone_diffusion_type: Optional[TypeCOG]
+    presentation_resume: Optional[
+        Annotated[str, StringConstraints(max_length=280)]
+    ] = None
+    presentation_detail: Optional[str] = None
+    types: Optional[list[TypologieService]] = None
+    thematiques: Optional[list[Thematique]] = None
+    prise_rdv: Optional[HttpUrl] = None
+    frais: Optional[list[Frais]] = None
+    frais_autres: Optional[str] = None
+    profils: Optional[list[Profil]] = None
+    pre_requis: Optional[str] = None
+    cumulable: Optional[bool] = None
+    justificatifs: Optional[str] = None
+    formulaire_en_ligne: Optional[HttpUrl] = None
+    commune: Optional[str] = None
+    code_postal: Optional[
+        Annotated[
+            str, StringConstraints(min_length=5, max_length=5, pattern=r"^\d{5}$")
+        ]
+    ] = None
+    code_insee: Optional[
+        Annotated[str, StringConstraints(min_length=5, max_length=5)]
+    ] = None
+    adresse: Optional[str] = None
+    complement_adresse: Optional[str] = None
+    longitude: Optional[float] = None
+    latitude: Optional[float] = None
+    recurrence: Optional[str] = None
+    date_creation: Optional[date] = None
+    date_suspension: Optional[date] = None
+    lien_source: Optional[HttpUrl] = None
+    telephone: Optional[str] = None
+    courriel: Optional[EmailStr] = None
+    contact_public: Optional[bool] = None
+    date_maj: Optional[date | datetime] = None
+    modes_accueil: Optional[list[ModeAccueil]] = None
+    zone_diffusion_type: Optional[TypeCOG] = None
     zone_diffusion_code: Optional[
-        constr(regex=r"^\w{5}$")  # code commune
-        | constr(regex=r"^\d{9}$")  # code epci
-        | constr(regex=r"^\w{2,3}$")  # code departement
-        | constr(regex=r"^\d{2}$")  # code region
-    ]
-    zone_diffusion_nom: Optional[str]
-    contact_nom_prenom: Optional[str]
-    modes_orientation_beneficiaire: Optional[list[ModeOrientationBeneficiaire]]
-    modes_orientation_accompagnateur: Optional[list[ModeOrientationAccompagnateur]]
-
-    class Config:
-        extra = Extra.forbid
+        Annotated[str, StringConstraints(pattern=r"^\w{5}$")]  # code commune
+        | Annotated[str, StringConstraints(pattern=r"^\d{9}$")]  # code epci
+        | Annotated[str, StringConstraints(pattern=r"^\w{2,3}$")]  # code departement
+        | Annotated[str, StringConstraints(pattern=r"^\d{2}$")]  # code region
+    ] = None
+    zone_diffusion_nom: Optional[str] = None
+    contact_nom_prenom: Optional[str] = None
+    modes_orientation_beneficiaire: Optional[list[ModeOrientationBeneficiaire]] = None
+    modes_orientation_accompagnateur: Optional[
+        list[ModeOrientationAccompagnateur]
+    ] = None
 
 
 class Structure(BaseModel):
+    model_config = ConfigDict(extra="forbid")
+
+    # fields
     id: str
-    siret: Optional[constr(min_length=14, max_length=14, regex=r"^\d{14}$")]
-    rna: Optional[constr(min_length=10, max_length=10, regex=r"^W\d{9}$")]
+    siret: Optional[
+        Annotated[
+            str, StringConstraints(min_length=14, max_length=14, pattern=r"^\d{14}$")
+        ]
+    ] = None
+    rna: Optional[
+        Annotated[
+            str, StringConstraints(min_length=10, max_length=10, pattern=r"^W\d{9}$")
+        ]
+    ] = None
     nom: str
     commune: str
-    code_postal: constr(min_length=5, max_length=5, regex=r"^\d{5}$")
-    code_insee: Optional[constr(min_length=5, max_length=5)]
+    code_postal: Annotated[
+        str, StringConstraints(min_length=5, max_length=5, pattern=r"^\d{5}$")
+    ]
+    code_insee: Optional[
+        Annotated[str, StringConstraints(min_length=5, max_length=5)]
+    ] = None
     adresse: str
-    complement_adresse: Optional[str]
-    longitude: Optional[float]
-    latitude: Optional[float]
-    typologie: Optional[Typologie]
-    telephone: Optional[str]
-    courriel: Optional[EmailStr]
-    site_web: Optional[HttpUrl]
-    presentation_resume: Optional[constr(max_length=280)]
-    presentation_detail: Optional[str]
-    source: Optional[str]
+    complement_adresse: Optional[str] = None
+    longitude: Optional[float] = None
+    latitude: Optional[float] = None
+    typologie: Optional[Typologie] = None
+    telephone: Optional[str] = None
+    courriel: Optional[EmailStr] = None
+    site_web: Optional[HttpUrl] = None
+    presentation_resume: Optional[
+        Annotated[str, StringConstraints(max_length=280)]
+    ] = None
+    presentation_detail: Optional[str] = None
+    source: Optional[str] = None
     date_maj: date | datetime
     antenne: bool = False
-    lien_source: Optional[HttpUrl]
-    horaires_ouverture: Optional[str]
-    accessibilite: Optional[HttpUrl]
-    labels_nationaux: Optional[list[LabelNational]]
-    labels_autres: Optional[list[str]]
-    thematiques: Optional[list[Thematique]]
-
-    class Config:
-        extra = Extra.forbid
+    lien_source: Optional[HttpUrl] = None
+    horaires_ouverture: Optional[str] = None
+    accessibilite: Optional[HttpUrl] = None
+    labels_nationaux: Optional[list[LabelNational]] = None
+    labels_autres: Optional[list[str]] = None
+    thematiques: Optional[list[Thematique]] = None
 
 
 def generate_structures_json_schema():
     """Generate the structures file json schema from the `Structure` model."""
 
-    # json schema for a file containing a **list** of structures
+    adapter = pydantic.TypeAdapter(list[Structure])
+    schema = adapter.json_schema()
+
     return {
         "title": "Structures de l'insertion",
         "$schema": "http://json-schema.org/draft-07/schema",
@@ -111,20 +138,16 @@ def generate_structures_json_schema():
             "/main/structures.json"
         ),
         "description": "",
-        "type": "array",
-        "items": {
-            "$ref": "#/definitions/Structure",
-        },
-        # Pydantic generates the json schema of a **single** structure from the
-        # `Structure` model. Only the definitions are extracted.
-        "definitions": pydantic.schema_of(Structure)["definitions"],
+        **schema,
     }
 
 
 def generate_services_json_schema():
     """Generate the services file json schema from the `Service` model."""
 
-    # json schema for a file containing a **list** of services
+    adapter = pydantic.TypeAdapter(list[Service])
+    schema = adapter.json_schema()
+
     return {
         "title": "Services de l'insertion",
         "$schema": "http://json-schema.org/draft-07/schema",
@@ -133,11 +156,5 @@ def generate_services_json_schema():
             "/main/services.json"
         ),
         "description": "",
-        "type": "array",
-        "items": {
-            "$ref": "#/definitions/Service",
-        },
-        # Pydantic generates the json schema of a **single** service from the
-        # `Service` model. Only the definitions are extracted.
-        "definitions": pydantic.schema_of(Service)["definitions"],
+        **schema,
     }
