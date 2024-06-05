1bd220d84d04740c8690dff7c1e46a07c808cd87
Valentin Matton <taq@protonmail.ch>
2024-06-05 18:03:51 +0200

    chore: tri des profils

--- src/data_inclusion/schema/profils.py
+++ src/data_inclusion/schema/profils.py
@@ -2,29 +2,14 @@ from data_inclusion.schema.base import EnhancedEnum
 
 
 class Profil(EnhancedEnum):
-    SENIORS_65 = (
-        "seniors-65",
-        "Seniors (+ 65 ans)",
-        None,
-    )
-    FAMILLES_ENFANTS = (
-        "familles-enfants",
-        "Familles/enfants",
-        None,
-    )
     ADULTES = (
         "adultes",
         "Adultes",
         None,
     )
-    JEUNES_16_26 = (
-        "jeunes-16-26",
-        "Jeunes (16-26 ans)",
-        None,
-    )
-    PUBLIC_LANGUES_ETRANGERES = (
-        "public-langues-etrangeres",
-        "Public langues étrangères",
+    BENEFICIAIRES_RSA = (
+        "beneficiaires-rsa",
+        "Bénéficiaires du Revenu de Solidarité Active (RSA)",
         None,
     )
     DEFICIENCE_VISUELLE = (
@@ -32,9 +17,24 @@ class Profil(EnhancedEnum):
         "Déficience visuelle",
         None,
     )
-    SURDITE = (
-        "surdite",
-        "Surdité",
+    DEMANDEURS_EMPLOI = (
+        "demandeurs-demploi",
+        "Demandeurs ou demandeuses d’emploi",
+        None,
+    )
+    FAMILLES_ENFANTS = (
+        "familles-enfants",
+        "Familles/enfants",
+        None,
+    )
+    FEMMES = (
+        "femmes",
+        "Femmes",
+        "Le lieu propose des accompagnements réservés aux femmes.",
+    )
+    HANDICAPS_MENTAUX = (
+        "handicaps-mentaux",
+        "Handicaps mentaux : déficiences limitant les activités d’une personne",
         None,
     )
     HANDICAPS_PSYCHIQUES = (
@@ -45,23 +45,10 @@ class Profil(EnhancedEnum):
         ),
         None,
     )
-    HANDICAPS_MENTAUX = (
-        "handicaps-mentaux",
-        "Handicaps mentaux : déficiences limitant les activités d’une personne",
+    JEUNES_16_26 = (
+        "jeunes-16-26",
+        "Jeunes (16-26 ans)",
         None,
-    )
-    FEMMES = (
-        "femmes",
-        "Femmes",
-        "Le lieu propose des accompagnements réservés aux femmes.",
-    )
-    PERSONNES_HANDICAPEES = (
-        "personnes-handicapees",
-        "Personnes handicapées",
-        (
-            "Ce terme est privilégié pour désigner une incapacité persistante et "
-            "significative subie par une personne dans son environnement."
-        ),
     )
     PERSONNES_EN_SITUATION_DE_HANDICAP = (
         "personnes-en-situation-de-handicap",
@@ -77,13 +64,26 @@ class Profil(EnhancedEnum):
         "Personnes en situation d’illettrisme",
         None,
     )
-    BENEFICIAIRES_RSA = (
-        "beneficiaires-rsa",
-        "Bénéficiaires du Revenu de Solidarité Active (RSA)",
+    PERSONNES_HANDICAPEES = (
+        "personnes-handicapees",
+        "Personnes handicapées",
+        (
+            "Ce terme est privilégié pour désigner une incapacité persistante et "
+            "significative subie par une personne dans son environnement."
+        ),
+    )
+    PUBLIC_LANGUES_ETRANGERES = (
+        "public-langues-etrangeres",
+        "Public langues étrangères",
         None,
     )
-    DEMANDEURS_EMPLOI = (
-        "demandeurs-demploi",
-        "Demandeurs ou demandeuses d’emploi",
+    SENIORS_65 = (
+        "seniors-65",
+        "Seniors (+ 65 ans)",
+        None,
+    )
+    SURDITE = (
+        "surdite",
+        "Surdité",
         None,
     )
